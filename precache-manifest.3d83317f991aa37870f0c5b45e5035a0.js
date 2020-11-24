4cfe0684f8741500797b209071916621c0f0ca9c
Jane You <janeyou@Janes-MacBook-Pro.local>
2020-11-23 21:39:34 -0500

    Updates

---
+++ precache-manifest.3d83317f991aa37870f0c5b45e5035a0.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "226f93e7f647af563a6d3770c14f8032",
+    "url": "/Jane3/Burger-Builder/index.html"
+  },
+  {
+    "revision": "b69df63160c53b835a6c",
+    "url": "/Jane3/Burger-Builder/static/css/3.ca87188b.chunk.css"
+  },
+  {
+    "revision": "fc0289f812ddf4056b32",
+    "url": "/Jane3/Burger-Builder/static/css/main.ca25973c.chunk.css"
+  },
+  {
+    "revision": "7be61a8b58610244e78b",
+    "url": "/Jane3/Burger-Builder/static/js/2.06353dd7.chunk.js"
+  },
+  {
+    "revision": "b69df63160c53b835a6c",
+    "url": "/Jane3/Burger-Builder/static/js/3.94a28e7a.chunk.js"
+  },
+  {
+    "revision": "fc0289f812ddf4056b32",
+    "url": "/Jane3/Burger-Builder/static/js/main.5f49202f.chunk.js"
+  },
+  {
+    "revision": "3167f46bd4852b8d0deb",
+    "url": "/Jane3/Burger-Builder/static/js/runtime-main.5772f244.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/Jane3/Burger-Builder/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "674ec4b78a2024197b8ee7e6068dc4c1",
+    "url": "/Jane3/Burger-Builder/static/media/Burger-Builder.674ec4b7.png"
+  },
+  {
+    "revision": "fcd099e30198fb2aaa0e848e265c95fe",
+    "url": "/Jane3/Burger-Builder/static/media/Jane.fcd099e3.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/Jane3/Burger-Builder/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "5aace10e1f630704953ceb431ca1f024",
+    "url": "/Jane3/Burger-Builder/static/media/Skills.5aace10e.jpeg"
+  },
+  {
+    "revision": "e200e517ba161010c1dd889b334633a8",
+    "url": "/Jane3/Burger-Builder/static/media/bmo-logo.e200e517.jpg"
+  },
+  {
+    "revision": "8e290eda4f0f220e99457ba6b896d577",
+    "url": "/Jane3/Burger-Builder/static/media/contact.8e290eda.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Jane3/Burger-Builder/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "69f4b5d02eae292fafb47ecad21cdaf5",
+    "url": "/Jane3/Burger-Builder/static/media/sante-canada-.69f4b5d0.png"
+  },
+  {
+    "revision": "7ecae96481a46b5f290f27391c5c1704",
+    "url": "/Jane3/Burger-Builder/static/media/uOttawa.7ecae964.png"
+  }
+]);
