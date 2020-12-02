3bb7ecadd33af80733ca4bea5559b4837418dcf1
Jane You <janeyou@Janes-MacBook-Pro.local>
2020-12-02 11:39:40 -0500

    Updates

---
+++ build/precache-manifest.eefb234b764ebe493c8eb6f546097e46.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9271b3ddfbb8eb72604df2d680044745",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7e9e21c1f6386b27ff48",
+    "url": "/portfolio/static/css/3.ca87188b.chunk.css"
+  },
+  {
+    "revision": "1a460a9d4d7fab58f770",
+    "url": "/portfolio/static/css/main.49e2140f.chunk.css"
+  },
+  {
+    "revision": "5690d6b41f21db5912cc",
+    "url": "/portfolio/static/js/2.4b09794e.chunk.js"
+  },
+  {
+    "revision": "7e9e21c1f6386b27ff48",
+    "url": "/portfolio/static/js/3.3a6c4763.chunk.js"
+  },
+  {
+    "revision": "1a460a9d4d7fab58f770",
+    "url": "/portfolio/static/js/main.836dc1aa.chunk.js"
+  },
+  {
+    "revision": "e5f81b7d6a84322888fe",
+    "url": "/portfolio/static/js/runtime-main.dcbbe932.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "7e61f82437c44fa1ecf2331fdedb168d",
+    "url": "/portfolio/static/media/Burger-Builder.7e61f824.png"
+  },
+  {
+    "revision": "0c342b38359f333f52b9069e48d3cbce",
+    "url": "/portfolio/static/media/Jane.0c342b38.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "b624b450b89f44356ad52dfb2d6bdef9",
+    "url": "/portfolio/static/media/Recipe.b624b450.png"
+  },
+  {
+    "revision": "5aace10e1f630704953ceb431ca1f024",
+    "url": "/portfolio/static/media/Skills.5aace10e.jpeg"
+  },
+  {
+    "revision": "e200e517ba161010c1dd889b334633a8",
+    "url": "/portfolio/static/media/bmo-logo.e200e517.jpg"
+  },
+  {
+    "revision": "8e290eda4f0f220e99457ba6b896d577",
+    "url": "/portfolio/static/media/contact.8e290eda.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/portfolio/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "69f4b5d02eae292fafb47ecad21cdaf5",
+    "url": "/portfolio/static/media/sante-canada-.69f4b5d0.png"
+  },
+  {
+    "revision": "7ecae96481a46b5f290f27391c5c1704",
+    "url": "/portfolio/static/media/uOttawa.7ecae964.png"
+  }
+]);
