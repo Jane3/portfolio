428c1af5756edf00de1b0a97ab3c833287be8c9a
Jane You <janeyou@Janes-MacBook-Pro.local>
2020-12-18 23:58:58 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.fd36a05930aa151fe8534b6ca7d72653.js"
+  "/portfolio/precache-manifest.ba78c14be257565d3086c859ddc4cbac.js"
 );
 
 self.addEventListener('message', (event) => {
